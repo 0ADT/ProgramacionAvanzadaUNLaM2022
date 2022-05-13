f7d1e935e434d856150d1aab0afa9b264b14e313
0ADT <albertodtomadin@gmail.com>
2022-05-13 08:59:29 -0300

    Correccion de errores tipograficos

--- src/main/java/ejercicios_en_clase/luchadoresJaponeses/Main.java
+++ src/main/java/ejercicios_en_clase/luchadoresJaponeses/Main.java
@@ -1,37 +1,37 @@
 package ejercicios_en_clase.luchadoresJaponeses;
 
 /**
- * Descripción del problema
+ * Descripcion del problema
  * 
- * Una academia dedicada a las difíciles artes de lucha oriental, está
+ * Una academia dedicada a las dificiles artes de lucha oriental, esta�
  * organizando un torneo de luchadores sumo.
  * 
  * A los efectos de conformar la grilla de combates, es necesario armar parejas
- * de luchadores que puedan ofrecer un espectáculo atractivo. Como es sabido que
- * los cultores de esta disciplina son hombres de estructura física imponente,
+ * de luchadores que puedan ofrecer un espectaculo atractivo. Como es sabido que
+ * los cultores de esta disciplina son hombres de estructura fisica imponente,
  * los organizadores habitualmente arman duplas de combatientes estableciendo
  * comparaciones relativas a sus alturas y pesos.
  * 
- * Por experiencia, los organizadores saben que un combatiente ‘domina’ a otro
+ * Por experiencia, los organizadores saben que un combatiente "domina" a otro
  * si lo supera en ambas medidas, o bien si lo iguala en peso y lo supera en
  * altura, o viceversa. En cualquier otro caso, los luchadores no son
- * comparables lo que hace el resultado imprevisible y por lo tanto más
- * atractivo para el público.
+ * comparables lo que hace el resultado imprevisible y por lo tanto mas
+ * atractivo para el publico.
  * 
  * Sabiendo que no hay luchadores que coinciden en ambas medidas, los
  * organizadores quieren saber a cuantos posibles contrincantes domina cada uno
- * de ellos, y por esta razón se te pide que escribas un programa que efectúe
+ * de ellos, y por esta razon se te pide que escribas un programa que efectue
  * este recuento
  * 
  * Datos de entrada 
  * Se recibe un archivo sumo.in con el siguiente formato: 
- * 	-Una línea que indica la cantidad L (1 ≤ L ≤ 100.000) de luchadores. 
- * 	-L líneas con 2 números P y H (0 ≤ P,H ≤ 1.000.000) que indican el peso y la altura de cada
+ * 	-Una linea que indica la cantidad L (1 <= L <= 100.000) de luchadores. 
+ * 	-L lineas con 2 numeros P y H (0 <= P,H <= 1.000.000) que indican el peso y la altura de cada
  * participante, separados por blancos.
  * 
  * Datos de salida
  * Se debe generar un archivo sumo.out conteniendo
- * 	-L líneas con la cantidad de luchadores a quienes domina cada participante, en el mismo
+ * 	-L lineas con la cantidad de luchadores a quienes domina cada participante, en el mismo
  * orden en el que los participantes entraron.
  * 
  * Ejemplo Para la entrada: 
--- src/main/java/ejercicios_loom/oia2/MainMesadas.java
+++ src/main/java/ejercicios_loom/oia2/MainMesadas.java
@@ -11,13 +11,13 @@ package ejercicios_loom.oia2;
  * inferior. Es decir, la mesada superior debe tener lados que son iguales o menores que la mesada
  * inferior. Antes de apilarse, las mesadas pueden ser rotadas 90� en cualquier sentido, con lo
  * cual una mesada de 2 x 5 puede apilarse sobre una mesada de 7 x 2. Se desea saber cual es la cantidad
- * mínima de pilas de mesadas que se puede conseguir. Se te pide que escribas un programa que determinen
+ * minima de pilas de mesadas que se puede conseguir. Se te pide que escribas un programa que determinen
  * el numero minimo de pilas.
  * 
  * ++++Datos de entrada 
  * Se recibe un archivo mesadas.in con el siguiente formato:
- * Una linea que indica la cantidad N ( 1 ≤ N ≤ 200.000 ) de mesadas de granito
- * N lineas con 2 numeros a y b ( 0 < a, b ≤ 1.000.000 ) que indican las dimensiones de las mesadas
+ * Una linea que indica la cantidad N ( 1 <= N <= 200.000 ) de mesadas de granito
+ * N lineas con 2 numeros a y b ( 0 < a, b <= 1.000.000 ) que indican las dimensiones de las mesadas
  * 
  * ++++Datos de salida 
  * Se debe generar un archivo mesadas.out conteniendo una linea con la cantidad minima de pilas
@@ -33,7 +33,7 @@ package ejercicios_loom.oia2;
  * 2 7
  * 
  * La salida mesadas.out deberia ser:
- * 3 * 
+ * 3
  **/
 public class MainMesadas {
 
