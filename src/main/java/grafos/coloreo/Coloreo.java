ba00ee25e8000ec2f9637bc6461aba720100a925
0ADT <albertodtomadin@gmail.com>
2022-06-17 14:41:47 -0300

    Agregadas las clases para coloreo

--- src/main/java/grafos/coloreo/Coloreo.java
+++ src/main/java/grafos/coloreo/Coloreo.java
@@ -1,5 +1,18 @@
 package grafos.coloreo;
 
 public class Coloreo {
+	private int colores;
+	private int[] coloresAsignados;
 
+	public int getColores() {
+		return colores;
+	}
+
+	public int[] getColoresAsignados() {
+		return coloresAsignados;
+	}
+
+	public int getColorDelNodo(int nodo) {
+		return coloresAsignados[nodo];
+	}
 }
---
+++ src/main/java/grafos/coloreo/ColoreoPorColor.java
@@ -0,0 +1,14 @@
+package grafos.coloreo;
+
+import grafos.Grafo;
+import grafos.coloreo.orden.Orden;
+
+public class ColoreoPorColor implements MetodoColoreo {
+
+	@Override
+	public Coloreo Pintar(Grafo g, Orden o) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
